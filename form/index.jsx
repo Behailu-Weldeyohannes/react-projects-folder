e41a913f685ef46fb9669d9eb26c174dfda9f795
Behailu Weldeyohannes <bweldeyohannes@gmail.com>
2023-02-14 14:42:55 -0500

    form project added

---
+++ form/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FormNote from "./components/FormNote";
+import App from "./App";
+
+// ReactDOM.render(<Form />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <FormNote /> */}
+    <App />
+  </React.StrictMode>
+);
